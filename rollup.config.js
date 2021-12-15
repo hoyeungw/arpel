4bb59fa48497b492d1b6754e757a603a0fac7087
Hoyeung <hoyeungw@outlook.com>
2021-12-15 19:22:41 +0800

    create arpel

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import babel               from '@rollup/plugin-babel'
+import commonjs            from '@rollup/plugin-commonjs'
+import json                from '@rollup/plugin-json'
+import nodeResolve         from '@rollup/plugin-node-resolve'
+import { decoObject, ros } from '@spare/logger'
+import fileInfo            from 'rollup-plugin-fileinfo'
+
+const { name, dependencies, exports, module } = require(process.cwd() + '/package.json')
+
+console.log(ros('Executing'), name, process.cwd())
+console.log(ros('Dependencies'), decoObject(dependencies || {}, { bracket: true }))
+
+export default [
+  {
+    input: 'index.js',
+    external: Object.keys(dependencies || {}),
+    output: [
+      { file: exports.import, format: 'esm' },  // ES module (for bundlers) build.
+      { file: exports.require, format: 'cjs' }  // CommonJS (for Node) build.
+    ],
+    plugins: [
+      nodeResolve({ preferBuiltins: true }),
+      commonjs({ include: /node_modules/ }),
+      babel({
+        babelrc: false,
+        comments: true,
+        sourceMap: true,
+        exclude: 'node_modules/**',
+        babelHelpers: 'bundled',
+        presets: [
+          [ '@babel/preset-env', { targets: { node: '16' } } ]
+        ],
+        plugins: [
+          [ '@babel/plugin-proposal-pipeline-operator', { proposal: 'minimal' } ],
+        ]
+      }),
+      json(),
+      fileInfo()
+    ]
+  }
+]
